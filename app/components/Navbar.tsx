14cdc5da0406678d625b80e63e10d7513f2a3c0d
Angela <ing.angela.mazo@gmail.com>
2025-03-19 22:17:10 -0500

    Feat: Home component creation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,11 +6,11 @@ export default function Navbar (){
         <div className="flex space-x-6">
             <Link className="hover: underline" to={"/"}>Inicio</Link>
             <Link className="hover: underline" to={"/"}>Servicios</Link>
-            <Link className="hover: underline" to={"/"}>Oportunidades</Link>    
+            <Link className="hover: underline" to={"/"}>Oportunidades</Link>
         </div>
         <div className="flex space-x-4">
             <Link className="bg-white text-blue-900 rounded-full px-4 py-1" to={"/"}>Login</Link>
-            <button className="bg-pink-500 rounded-full px-4 py-1">Registrate</button>            
+            <button className="bg-pink-500 rounded-full px-4 py-1">Registrate</button>
         </div>
     </nav>
    );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,23 @@
 import type { MetaFunction } from "@remix-run/node";
 import Navbar from "~/components/Navbar";
+import Home from "~/components/Home";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Antivirus" },
-    
   ];
 };
 
 export default function Index() {
   return (
     <div className="font-sans bg-gray-100">
-      <Navbar></Navbar>
+      <Navbar />
+      <Home />
     </div>
-    )}
+  );
+}
+
+
+    
+
+   
