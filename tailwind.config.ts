05375d83d36ebeb5009ddcc664324705239983d5
Melisa <melissaserna19@gmail.com>
2025-03-29 20:55:39 -0500

    Merge completado resolviendo conflictos con con login.module.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,24 @@
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         inter: ["Inter", "sans-serif"],
+//         poppins: ["Poppins", "sans-serif"],
+//         raleway: ["Raleway", "sans-serif"],
+//         nunito: ["Nunito", "sans-serif"],
+//         montserrat: ["Montserrat", "sans-serif"],
+//         impact: ["Impact", "sans-serif"],
+//         reddit: ["Reddit Sans", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/typography")],
+// } satisfies Config;
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 import typography from "@tailwindcss/typography";
 
 const config: Config = {
